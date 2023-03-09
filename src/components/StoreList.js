9c998f6ed06cece16066862449b74e822d1bd834
Simon White <simon@octave.nz>
2023-03-09 13:40:51 +1300

    add featured store to list and favs pages

--- src/components/StoreList.js
+++ src/components/StoreList.js
@@ -4,6 +4,9 @@ import Intro from "./Intro";
 import Store from "./Store";
 import Adverts from "./Adverts";
 import Footer from "./Footer";
+import FeaturedStore from "./FeaturedStore";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 class StoreList extends React.Component {
 
@@ -42,19 +45,19 @@ class StoreList extends React.Component {
     } else if (region === "CentralNorth") {
       for (var l = 0; l < centralNorthStores.length; l++) {
         centralNorthStores[l].style.display = "block";
-      }  
+      }
     } else if (region === "Wellington") {
       for (var m = 0; m < wellingtonStores.length; m++) {
         wellingtonStores[m].style.display = "block";
-      }  
+      }
     } else if (region === "SouthIsland") {
       for (var n = 0; n < southIslandStores.length; n++) {
         southIslandStores[n].style.display = "block";
-      } 
+      }
     } else if (region === "OnlineOnly") {
       for (var o = 0; o < onlineOnlyStores.length; o++) {
         onlineOnlyStores[o].style.display = "block";
-      } 
+      }
     }
   };
 
@@ -64,6 +67,49 @@ class StoreList extends React.Component {
       <div className="store-list">
         <Header title="Vinyl Directory" />
         <Intro insideList={true} />
+
+        <Carousel showStatus={false} autoPlay={true} interval={10000} showArrows={false}>
+                <div>
+                    {Object.keys(this.props.stores).map((key) => (
+                      <FeaturedStore
+                        key={key}
+                        index={key}
+                        details={this.props.stores[key]}
+                        addToFavourites={this.props.addToFavourites}
+                        favourites={this.props.favourites}
+                      />
+                    ))}
+                </div>
+                 <div>
+                     <div className="store store--featured col-sm-12">
+                        <div className="row">
+                            <h3>Featured store</h3>
+                            <img src="https://vinyl-dir.s3.ap-southeast-2.amazonaws.com/Vinylrecords2.jpg" alt="vinyl records" className="image--featured"/>
+                            <div className="store-details">
+                                <h2 className="store__name">Your store here</h2>
+                                <address>
+                                    Your address displays here, along with your phone number, website link and any social media accounts.
+                              </address>
+                                <div className="store-text">
+                                    <p>Reach record collectors across New Zealand and beyond by becoming a featured store here. Contact us at info@vinyldirectory.nz to enquire now.</p>
+                                </div>
+                                <a href="mailto:info@vinyldirectory.nz">Email info@vinyldirectory.nz</a>
+                            </div>
+                        </div>
+                    </div>
+                 </div>
+                 <div>
+                    {Object.keys(this.props.stores).map((key) => (
+                      <FeaturedStore
+                        key={key}
+                        index={key}
+                        details={this.props.stores[key]}
+                        addToFavourites={this.props.addToFavourites}
+                        favourites={this.props.favourites}
+                      />
+                    ))}
+                </div>
+        </Carousel>
         <div>
         <h2>Store List</h2>
         <div className="filter-container">
